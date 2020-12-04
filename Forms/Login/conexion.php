8539741177ba158c299c4653642b29368091e6a8
DavidGL99 <75373483+DavidGL99@users.noreply.github.com>
2020-12-04 19:06:08 +0100

    Delete conexion.php

--- Forms/Login/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('logs.php');
-$mysqli = new mysqli($host, $user, $password, $database);
-
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error . "<br/>";
-} 
-?>
