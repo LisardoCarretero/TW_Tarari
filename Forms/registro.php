8fcce624dc6ac9e2742d6e926c74d4a9dd38adca
lcc523 <lcc523@inlumine.ual.es>
2020-12-15 13:30:30 +0100

    esto funciona?

--- Forms/registro.php
+++ Forms/registro.php
@@ -1,18 +1,18 @@
-<!DOCTYPE html>
+<!DOCTYPE html>        <!-- control shift v previsualización -->
 <html lang="es" dir="ltr">
 
 <head>
     <meta charset="utf-8"><title>Formulario Registro</title>
-    <link rel="stylesheet" href="estilos/registro_css.css">
+    <link rel="stylesheet" href="estilos/registro.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container">
       <div class="recuadro">
-       
-        <form action="../Scripts/script-singup.php" method="post">
-        <img src="../../Icons/icon_user2.png"class="icon_user">
+        <img class="icon_user" src="../Icons/icon_user2.png" width=15% height=15%
+        alt="El icono de Usuario">
+        <form action="scripts/script-singup.php" method="post">
           <h5><p class="cabezera">Regístrate</p></h5>
           <div class="form-group row">
            <label for="inputNombre" class="col-sm-2 col-form-label"><p class="define_box" name="nombre"></p></label>
--- Forms/scripts/script-singup.php
+++ Forms/scripts/script-singup.php
@@ -11,15 +11,22 @@
     $Usuario = $_POST['Usuario'];
     $Telefono = $_POST['telf'];
 
+
+    //Comprobar si confirmaciones son iguales, si no lanzar a una nueva pagina 
     $consultaSQL = "SELECT * from usuario WHERE usuario.Username = '$Usuario' AND usuario  .contrasena = '$Password'";
     $consulta = $mysqli->query($consultaSQL);
     
     if($consulta->num_rows ==0){
+
+        
         $Insert = "INSERT INTO Usuario (Username, nombre, apellidos, correo, contrasena, telefono, TipoUsuario)
         VALUES ('$Usuario', '$Nombre', '$Apellidos', '$Email', '$Password', '$Telefono', '0')";
         
         $consulta_insert = $mysqli->query($Insert);
+
+        //Lanzar a index.html
     }else {
         echo("Usuario ya registrado");
+        //Implementar como en login error       
     }
 ?>
